380839b15b2f2ccb5f8dbc9e09ea29d32a7f1389
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-09-14 18:38:31 -0400

    Initial commit

---
+++ src/main/java/io/github/thepoultryman/rustic_craft_example/RusticCraftExample.java
@@ -0,0 +1,15 @@
+package io.github.thepoultryman.rustic_craft_example;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RusticCraftExample implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Rustic Craft Example Mod");
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("The Rustic Craft Example mod is initializing.");
+	}
+}
